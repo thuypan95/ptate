ffcde225256f0a5107459168bf25da6675384a1c
Thuy Dev <thuypan95@gmail.com>
2023-09-18 13:40:37 +0700

    uodarte

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import moment from "moment";
 import "react-circular-progressbar/dist/styles.css";
-import chicken from "./images/chicken.jpg";
-import big from "./images/big.jpg";
+// import chicken from "./images/chicken.jpg";
+// import big from "./images/big.jpg";
 import ate from "./images/ate.png";
 import small from "./images/small.jpg";
+import {listDN} from "./danhngon"
 
 function App() {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [time, setTime] = useState(Date.now());
+  const [index, setIndex] = useState<number>(Math.floor(Math.random() * 40) + 1)
   const start1 = moment("2023-02-14");
   var current = moment().startOf("day");
 
@@ -35,59 +37,43 @@ function App() {
   var seconds = end.diff(start, "seconds");
 
   const total = moment.duration(current.diff(start1)).asDays();
-
   useEffect(() => {
-    const interval = setInterval(() => setTime(Date.now()), 1000);
+    const interval = setInterval(() => {
+     
+      const rndInt = Math.floor(Math.random() * 40) + 1;
+      console.log('rndInt', rndInt)
+      setIndex(rndInt)
+
+    }, 60000);
     return () => {
       clearInterval(interval);
     };
   }, []);
-  // const percent = (total / 365) * 100;
   const list = [
     { type: "years", value: years },
     { type: "months", value: months },
     { type: "days", value: days },
     { type: "hours", value: hours },
     { type: "minutes", value: minutes },
-    { type: "seconds", value: seconds },
+    { type: "seconds", value: '00' },
   ];
   return (
     <div>
       
       <div className="App">
       <div className="small">
-          <img src={small} alt="small" width={200} />
+          <img src={small} alt="small" width={150} />
         </div>
-        {/* <CircularProgressbarWithChildren styles={{
-          path: {
-            // Path color
-            // stroke: `rgba(62, 152, 199, ${percent / 100})`,
-            // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
-            strokeLinecap: 'butt',
-            // Customize transition animation
-            transition: 'stroke-dashoffset 0.5s ease 0s',
-            // Rotate the path
-            transform: 'rotate(0.25turn)',
-            transformOrigin: 'center center',
-          },
-              root: {width:200},
-               trail:{stroke:"#F4D160"},
-
-        }} value={percent} strokeWidth={4}
-        >
-          <div className="content">
-            <h2>{total}</h2>
-            <h2>{total}</h2>
-          </div>
-         <div className='days'> days</div>
-        </CircularProgressbarWithChildren> */}
+        
         <div className="anniversary">
           <h3>{total}</h3>
           <p>Days</p>
         </div>
-        <div className="big-chicken">
-          <img src={big} alt="big" width={100} />
-          <img src={chicken} alt="big" width={100} />
+        <div className="">
+          {/* <img src={big} alt="big" width={100} />
+          <img src={chicken} alt="big" width={100} /> */}
+          <blockquote>"{listDN[index].d}"</blockquote>
+          <blockquote style={{fontFamily:'sans-serif', fontSize:'13px', fontStyle:'italic'}}>{listDN[index].vn}</blockquote>
         </div>
         <div style={{width:"100%"}}>
         <div className="wrapper">
---
+++ src/danhngon.js
@@ -0,0 +1,165 @@
+export const listDN =[{
+    "d": "Wer das ABC recht kann, hat die schwerste Arbeit getan.",
+    "vn": "Ai có thể làm những việc cơ bản thì người đó đã làm xong những công việc khó nhất."
+},
+{
+    "d": "Bei Nacht sind alle Katzen grau.",
+    "vn": "Ban đêm nhà ngói cũng như nhà tranh."
+},
+{
+    "d": "Absicht ist die Seele der Tat",
+    "vn": "Ý định là linh hồn của hành động."
+},
+{
+    "d": "Der Kluge wartet die Zeit ab.",
+    "vn": "Người khôn biết chờ thời."
+},
+{
+    "d": "Was bald zunimmt, das nimmt bald ab.",
+    "vn": "Dục tốc bất đạt."
+},
+{
+    "d": "Alles hat seine Grenzen.",
+    "vn": "Tất cả đều có giới hạn của nó."
+},
+{
+    "d": "Alles hat sein Warum",
+    "vn": "Tất cả đều có lý do của nó!"
+},
+{
+    "d": "Wo in Wille ist, ist auch ein Weg.",
+    "vn": "Ở đâu có một lý do, ở đó có một con đường."
+},
+{
+    "d": "Sage nicht alles, was du weißt. Aber wisse alles, was du sagst.",
+    "vn": "Đừng nói tất cả những gì mình biết. Nhưng phải biết những gì mình nói."
+},
+{
+    "d": "Alles vergeht, Wahrheit besteht.",
+    "vn": "Tất cả sẽ trôi qua , nhưng sự thật sẽ tồn tại mãi."
+},
+{
+    "d": "Alt genug und doch nicht klug",
+    "vn": "Già rồi mà không khôn."
+},
+{
+    "d": "Alte Liebe rostet nicht.",
+    "vn": "Tình già không phai."
+},
+{
+    "d": "Das Alter soll man ehren.",
+    "vn": "Kính lão đắc thọ."
+},
+{
+    "d": "Andere Länder, andere Sitte.",
+    "vn": "Nước nào phong tục đó."
+},
+{
+    "d": "Eine Hand Wäscht die andere",
+    "vn": "Lá lành đùm lá rách."
+},
+{
+    "d": "Alle Anfang ist schwer",
+    "vn": "Vạn sự khởi đầu nan."
+},
+{
+    "d": "Guter Anfang, gutes Ende",
+    "vn": "Bắt đầu tốt, kết thúc tốt."
+},
+{
+    "d": "Der eine Sät, der andere erntet",
+    "vn": "Người này gieo, kẻ khác gặt."
+},
+{
+    "d": "Glücklich ist, wer vergißt, was doch nicht zu ändern ist.",
+    "vn": "Hạnh phúc là người biết quên đi điều không thể thay đổi được."
+},
+{
+    "d": "Arbeit macht das Leben süß.",
+    "vn": "Công việc làm cuộc sống tươi đẹp."
+},
+{
+    "d": "Arbeit ist das Lebens Würze.",
+    "vn": "Công việc là gia vị của cuộc sống."
+},
+{
+    "d": "Arm oder reich, der Tod macht alles gleich.",
+    "vn": "Giàu hay nghèo, ai cung phai chet."
+},
+{
+    "d": "Bittere Arzneien sind die wirksamsten.",
+    "vn": "Thuốc đắng giã tật."
+},
+{
+    "d": "Mäßigkeit ist die beste Arznei.",
+    "vn": "Sự điều độ là liều thuốc tốt nhất."
+},
+{
+    "d": "Die Zeit heilt alle Wunden.",
+    "vn": "Thời gian chữa lành mọi vết thương."
+},
+{
+    "d": "Aus dem Augen, aus dem Sinn.",
+    "vn": "Xa mặt cách lòng."
+},
+{
+    "d": "Die Augen sind der Spiegel der Seele.",
+    "vn": "Đôi mắt là cửa sổ của tâm hồn."
+},
+{
+    "d": "Viele Bächer machen einen Fluß.",
+    "vn": "Tích tiểu thành đại"
+},
+{
+    "d": "Bellende Hund beißen nicht.",
+    "vn": "Chó sủa là chó không cắn."
+},
+{
+    "d": "Liebe kann Berge versetzen.",
+    "vn": "Tình yêu có thể dời núi non."
+},
+{
+    "d": "Je weiser, je bescheidener.",
+    "vn": "Càng khôn ngoan càng khiêm tốn."
+},
+{
+    "d": "Einmal sehen ist besser als zehnmal hören",
+    "vn": "Trăm nghe không bằng một thấy."
+},
+
+{
+    "d": "Den Baum muß man biegen, solange er jung ist.",
+    "vn": "Uốn tre từ lúc còn là măng"
+},
+{
+    "d": "Ein faules Eis verdirbt den ganzen Brei",
+    "vn": "Một con sâu làm rầu nồi canh."
+},
+{
+    "d": "Dank ist der beste Klang.",
+    "vn": "Cám ơn là âm thanh ngọt ngào nhất."
+},
+{
+    "d": "Wie der Topf, so der Deckel.",
+    "vn": "Nồi nào úp vung đấy."
+},
+{
+    "d": "Wer im Sommer nicht erntet, muß im Winter derben.",
+    "vn": "Ai không biết lo xa, tất có ưu hoàn gần."
+},
+{
+    "d": "Denke zweimal, ehe du einmal sprichst",
+    "vn": "Suy nghĩ hai lần trước khi nói."
+},
+{
+    "d": "Der Mensch denkt, Gott lenkt",
+    "vn": "Mưu sự tại nhân, thành sự tại thiên."
+},
+{
+    "d": "Auch ein Kluger Dieb wird einmal gefangen.",
+    "vn": "Đi đêm hoài cũng có ngày gặp ma."
+},
+{
+    "d": "Geld ist ein gutter Diener, aber ein böser Herr",
+    "vn": "Tiền bạc là đầy tớ tốt nhưng là ông chủ xấu."
+},]
